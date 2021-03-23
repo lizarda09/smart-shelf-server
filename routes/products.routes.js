5be7ab69754e8ee041ef05f872cec59dc5b3c640
Lizarda <mirliz3748@gmail.com>
2021-03-23 23:52:39 +0200

    added complited business logic

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -21,25 +21,39 @@ router.post('/:id/checkDiscount', async function (req, res){
     try {
         const id = req.params.id;
         const product = await Product.findById(id);
-        const {dateOfManufacture, shelfLife} = product;
+        const {dateOfManufacture, price, shelfLife} = product;
         const today = new Date();
         const arrDateOfManufacture = dateOfManufacture.split('-');
         const dayOfExpiration = +arrDateOfManufacture[2] + (shelfLife/24);
         const dateOfExpiration = new Date(Date.parse(`${+arrDateOfManufacture[0]}-${+arrDateOfManufacture[1]}-${dayOfExpiration + 1}`));
         const diff = Math.round((dateOfExpiration - today)/(1000*60*60*24));
+        let newPrice, percent;
         switch (diff) {
             case 0:
-                res.status(200).json({message: 'скидка 50%'});
+                newPrice = +price / 2;
+                await Product.updateOne({_id: id}, { $set: { price: newPrice }});
+                res.status(200).json({message: 'Скидка 50%'});
                 break;
             case 1:
-                res.status(200).json({message: 'скидка 30%'});
+                percent = +price / 100 * 30;
+                newPrice = price - percent;
+                await Product.updateOne({_id: id}, { $set: { price: newPrice }});
+                res.status(200).json({message: 'Скидка 30%'});
                 break;
             case 2:
-                res.status(200).json({message: 'скидка 15%'});
+                percent = +price / 100 * 15;
+                newPrice = price - percent;
+                await Product.updateOne({_id: id}, { $set: { price: newPrice }});
+                res.status(200).json({message: 'Скидка 15%'});
                 break;
             case 3:
-                res.status(200).json({message: 'скидка 10%'});
+                percent = +price / 10;
+                newPrice = price - percent;
+                await Product.updateOne({_id: id}, { $set: { price: newPrice }});
+                res.status(200).json({message: 'Скидка 10%'});
                 break;
+            default:
+                res.status(200).json({message: 'Скидки нет'});
         }
     } catch (e) {
         res.status(500).json({message: 'Что-то пошло не так...'});
