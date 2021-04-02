584deed5344c9296f23ed1cd8221fa1d851cfe28
lizarda09 <70509958+lizarda09@users.noreply.github.com>
2021-04-02 04:23:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-/*const MongoClient = require("mongodb").MongoClient;
-
-const url = "mongodb://localhost:27017/";
-const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
-
-mongoClient.connect(function(err, client){
-
-    const db = client.db("usersdb");
-    const collection = db.collection("users");
-    let user = {name: "Tom", age: 23, education: "student"};
-    collection.insertOne(user, function(err, result){
-
-        if(err){
-            return console.log(err);
-        }
-        console.log(result.ops);
-        client.close();
-    });
-});
-
- */
-
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
